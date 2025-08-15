58ebb3941c44e616ccff445b3a65fc3f00e59322
sarikun19830407 <sarikunikun07@gmail.com>
2025-08-15 17:34:08 +0700

    Pesan commit kamu di sini

--- app_cbt/view/view_admin.py
+++ app_cbt/view/view_admin.py
@@ -73,9 +73,10 @@ def app_cbt (request):
 @user_passes_test(lambda user: user.is_superuser, login_url=settings.LOGIN_URL)
 @csrf_protect
 def home (request):
+    lembaga = models.Lembaga.objects.filter(status=True).first()
     Data = models.TahunPelajaran.objects.filter(status=True, Nama_User=request.user)
-    Siswa = models.Pengguna.objects.filter(is_siswa=True, Nama_User=request.user, Nama_Lembaga = request.user.Nama_Lembaga)
-    rombel = models.Rombel_kelas.objects.filter(Nama_User=request.user, Nama_Lembaga = request.user.Nama_Lembaga)
+    Siswa = models.Pengguna.objects.filter(is_siswa=True, Nama_User=request.user, Nama_Lembaga=lembaga)
+    rombel = models.Rombel_kelas.objects.filter(Nama_User=request.user, Nama_Lembaga=lembaga)
     contex={
         "data":"Home",
         "judul":"CBT",
