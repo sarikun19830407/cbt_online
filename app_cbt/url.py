aa0c04d057323c3c3a774592c454515f7cdc3117
sarikun <sarikunikun07@gmail.com>
2025-12-19 23:29:48 +0700

    Pesan commit kamu di sini

--- app_cbt/url.py
+++ app_cbt/url.py
@@ -140,9 +140,7 @@ urlpatterns =[
     path('selesai_ujian/<str:kode_soal>/selesai_ujian/', view_siswa.selesai_ujian, name='selesai_ujian'),
     
     # urls.py
-    path('autosave/', view_siswa.autosave_jawaban, name='autosave_jawaban'),
-    path('submit_semua_jawaban/', view_siswa.submit_semua_jawaban, name='submit_semua_jawaban'),
-    path('submit_batch_jawaban/', view_siswa.submit_batch_jawaban, name='submit_batch_jawaban'),
+    
 
 
 
